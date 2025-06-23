5b2f5cfd10f0f02e722eb01f7f2d4aa03eb79899
Prath1997 <prathameshdhondge97@gmail.com>
2025-06-23 23:03:22 +0530

    Testcases updated | 23 june 2025

--- src/main/java/driver/WebdriverSetup.java
+++ src/main/java/driver/WebdriverSetup.java
@@ -7,7 +7,6 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class WebdriverSetup {
 
-<<<<<<< HEAD
 	public WebDriver driver;
 	
 	String BuildPath = "https://www.amazon.in/";
@@ -18,18 +17,6 @@ public class WebdriverSetup {
 		driver.manage().window().maximize();
 		driver.get(BuildPath);
 		return driver;
-=======
-	protected static WebDriver driver;
-	
-	String BuildPath = "https://www.amazon.in/";
-	
-	public void launchbrowser() {
-		WebDriverManager.chromedriver().setup();
-		driver=new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.get(BuildPath);
-		
->>>>>>> branch 'main' of https://github.com/Prath1997/Amazon.git
-	}
+
 	
 }
--- src/test/java/com/amazon/tests/LoginTest.java
+++ src/test/java/com/amazon/tests/LoginTest.java
@@ -1,6 +1,5 @@
 package com.amazon.tests;
 
-<<<<<<< HEAD
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
@@ -59,43 +58,4 @@ public class LoginTest extends WebdriverSetup {
 		
 	}
 
-=======
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
-
-import driver.WebdriverSetup;
-import pages.LoginPage;
-
-public class LoginTest extends WebdriverSetup {
-	
-    LoginPage loginAmazon;
-
-    @BeforeMethod
-    public void setUp() {
-        launchbrowser();
-        loginAmazon = new LoginPage(driver);
-    }
-    
-    @Test
-    private void LoginAmazon() {
-		// TODO Auto-generated method stub
-    	
-    	loginAmazon.Profile();
-    	loginAmazon.enterusername();
-    	loginAmazon.Continue();
-    	loginAmazon.enterPassword();
-    	loginAmazon.Signin();
-	}
-    
-	@Test(enabled = true)
-	private void Search() {
-
-		loginAmazon.ClickSearch();
-		loginAmazon.EnterSearch();
-		loginAmazon.Searchicon();
-		Assert.assertEquals(driver.getTitle(), "Amazon.in : iphone");
-	}
-    
-    
->>>>>>> branch 'main' of https://github.com/Prath1997/Amazon.git
 }
